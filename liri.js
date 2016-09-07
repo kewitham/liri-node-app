732897bbf094205926b72595ca18a9e728dd7de3
Katie Witham <kewitham@gmail.com>
2016-09-06 18:03:01 -0700

    added omdb

--- liri.js
+++ liri.js
@@ -54,14 +54,13 @@ else if (argthree = 'movie-this'){
 var request = require('request');
 
 // Then run a request to the OMDB API with the movie specified
-request('http://www.omdbapi.com/?t=' + argfour + '&y=&plot=short&r=json', function (error, response, body) {
+request('http://www.omdbapi.com/?t=' + argfour + '&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
 
 	// If the request is successful (i.e. if the response status code is 200)
 	if (!error && response.statusCode == 200) {
 
-		// Parse the body of the site and recover just the imdbRating
-		// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
-		console.log(JSON.parse(body).imdbRating)
+		// Parse the body of the site
+		console.log(JSON.parse(body).title + JSON.parse(body).year + JSON.parse(body).imdb.rating)
 	}
 });
 
